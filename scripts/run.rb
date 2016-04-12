e62d732d7e9ad2f83d4b57c799ab8ccc6c03abd5
Martin Schurig <martin@schurig.pw>
2016-04-12 15:30:10 +0200

    fix script

--- scripts/run.rb
+++ scripts/run.rb
@@ -15,9 +15,9 @@ Dir["#{project_root}/scripts/lib/*.rb"].each {|file| require file }
 @@config = YAML.load_file("#{project_root}/config.yml")
 script_start_time = Time.now
 
-# dc = DataCollector.new
-# dc.fetch!
-# dc.save_to_file!
+dc = DataCollector.new
+dc.fetch!
+dc.save_to_file!
 
 puts 'DONE!!! :)'.blue
 script_end_time = Time.now
