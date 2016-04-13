ef6ed63212c8424d6263968a11bc1b9cae7db166
Martin Schurig <martin@schurig.pw>
2016-04-13 17:21:19 +0200

    add charts

--- scripts/lib/facebook.rb
+++ scripts/lib/facebook.rb
@@ -1,7 +1,7 @@
 class Facebook
   def self.stats
     page = Nokogiri::HTML(open(@@config['facebook']['sharebutton_url']))
-    likes_count = page.css('.pluginCountTextConnected').text
+    likes_count = page.css('.pluginCountTextConnected').text.to_i
     { likes_count: likes_count }
   end
 end
