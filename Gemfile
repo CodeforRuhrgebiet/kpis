0720e75d4116c81e86fd70011c37dedc8282174f
Martin Schurig <martin@schurig.pw>
2016-12-19 15:04:55 +0100

    Remove crawler

--- Gemfile
+++ Gemfile
@@ -3,12 +3,3 @@ source 'https://rubygems.org'
 gem 'github-pages'
 gem 'jekyll'
 
-gem 'mime-types', "2.99.2"
-
-gem 'mechanize'
-gem 'colorize'
-gem 'nokogiri'
-gem 'json'
-gem 'twitter'
-gem 'firebase'
-gem 'firebase_token_generator'
--- scripts/lib/meetup.rb
+++
@@ -1,7 +0,0 @@
-class Meetup
-  def self.stats
-    url = "https://api.meetup.com/#{@@config['meetup']['group_slug']}?&sign=true&photo-host=public&only=members&key=#{@@config['meetup']['api_key']}"
-    api_result = JSON.load(open(url))
-    { members_count: api_result['members'] }
-  end
-end
